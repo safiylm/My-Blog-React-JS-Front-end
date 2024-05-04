3f08a8643af9236545056dd52a0d088e239950f3
safiylm <121391404+safiylm@users.noreply.github.com>
2024-05-04 11:14:53 +0200

    .

--- src/App.js
+++ src/App.js
@@ -1,14 +1,11 @@
 import Lifestyle from './components/PelliculePhotos/lifestyle.js'
 import LessonsforLife from './components/PelliculePhotos/nasihat.js'
-import Home from './components/Home.js'
-import Marifetullah from './components/PelliculePhotos/marifetullah.js'
-import Esma from './components/Esma/index.js'
-import Power from './components/Esma/power.js'
-import Superiority from './components/Esma/superiority.js'
-import IsmiAzam from './components/Esma/ismi-azam.js'
-import Merciful from './components/Esma/merciful.js'
-import Generous from './components/Esma/generous.js'
 import Citation from './components/PelliculePhotos/citation.js'
+import ManeviyatHome from './components/Esma/home.js'
+import Marifetullah from './components/Esma/marifetullah.js'
+import Esma from './components/Esma/index.js'
+
+
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
@@ -16,19 +13,16 @@ function App() {
     <BrowserRouter>
       <Routes>
         <Route path="/" >
-          <Route index element={<Home />} />
-          <Route path="/accueil" element={<Home />} />
-          <Route path="/lifestyle" element={<Lifestyle />} />
+          <Route index element={<Lifestyle />} />
+          <Route path="/accueil" element={<Lifestyle />} />
           <Route path="/lessonsforLife" element={<LessonsforLife />} />
-          <Route path="/marifetullah" element={<Marifetullah />} />
-          <Route path="/esma" element={<Esma />} />
-          <Route path="/esma/power" element={<Power />} />
-          <Route path="/esma/superiority" element={<Superiority />} />
-          <Route path="/esma/ismi-azam" element={<IsmiAzam />} />
-          <Route path="/esma/merciful" element={<Merciful />} />
-          <Route path="/esma/generous" element={<Generous />} />
           <Route path="/citation" element={<Citation />} />
-          
+
+
+          <Route path="/2108200015" element={<ManeviyatHome />} />
+          <Route path="/2108200015/marifetullah" element={<Marifetullah />} />
+          <Route path="/2108200015/esma" element={<Esma />} />
+
         </Route>
       </Routes>
     </BrowserRouter>
