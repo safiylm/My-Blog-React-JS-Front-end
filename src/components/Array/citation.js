9bc5ba49d443c402fc072c43135fc9155dfbafcc
safiylm <121391404+safiylm@users.noreply.github.com>
2024-05-27 21:54:04 +0200

    create array

---
+++ src/components/Array/citation.js
@@ -0,0 +1,38 @@
+
+export const quotes = [
+    {
+        "id": "1",
+        "image": ["https://images.pexels.com/photos/20225732/pexels-photo-20225732/free-photo-of-woman-holding-red-roses.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"],
+        "quote": {
+            "fr": ["La beauté commence au moment où vous décidez d'être vous-même."],
+            "en": ["Beauty begins the moment you decide to be yourself."],
+            "tr": ["Güzellik kendin olmaya karar verdiğin an başlar."],
+        }
+        , "auteur": "Coco Chanel "
+
+    },
+    {
+        "id": "2",
+        "image": ["https://images.pexels.com/photos/2114689/pexels-photo-2114689.jpeg?auto=compress&cs=tinysrgb&w=1200"],
+        "quote": {
+            "fr": ["Il faut toujours viser la lune car même en cas d'échec, on atterit dans les étoiles."],
+            "en": ["Shoot for the Moon. Even if you miss, you'll lang among the stars."],
+            "tr": ["Her zaman aya nişan almalısın çünkü başarısız olsan bile yıldızlara inersin."],
+        }
+        , "auteur": "Oscar Wilde"
+
+    }, {
+        "id": "3",
+        "image": ["https://images.pexels.com/photos/4482677/pexels-photo-4482677.jpeg?auto=compress&cs=tinysrgb&w=1200"],
+        "quote": {
+            "fr": ["Le bonheur n'est pas d'avoir tout ce que l'on désire mais d'apprécier ce que l'on a."],
+            "en": ["Happiness is not about getting all you want, it is about enjoying all you have. "],
+            "tr": ["Mutluluk her istediğinize sahip olmak değil, sahip olduklarınızın tadını çıkarmaktır..."],
+        }
+        , "auteur": "Paolo Coelho"
+
+    },];
+
+    export default {
+        quotes,
+    }
---
+++ src/components/Array/lifestyle.js
@@ -0,0 +1,179 @@
+import ph1 from '../../assets/ph1.jpg'
+import ph3 from '../../assets/ph3.jpg'
+import ph11 from '../../assets/ph11.jpg'
+import ph12 from '../../assets/ph12.jpg'
+import ph6 from '../../assets/ph6.jpg'
+import ph13 from '../../assets/ph13.jpg'
+import ph17 from '../../assets/ph17-1.jpg'
+import ph18 from '../../assets/ph18.jpg'
+import ph19 from '../../assets/ph19.jpg'
+import ph20 from '../../assets/ph20.jpg'
+import ph21 from '../../assets/ph21.jpg'
+import ph22 from '../../assets/ph22.png'
+import ph23 from '../../assets/ph23.jpg'
+import ph24 from '../../assets/ph24.jpg'
+import ph15 from '../../assets/ph15.jpg'
+
+import ph25 from '../../assets/ph25.jpg'
+import ph26 from '../../assets/ph26.jpg'
+import ph30 from '../../assets/ph30.jpg'
+
+
+export const array_lifestyle = [
+    {
+        "id": "ls1",
+        "image": [ph11],
+        "titre":
+        {
+            "fr": "",
+            "en": "Have minimalist life ",
+            "tr": "",
+        },
+        "texte":
+        {
+            "fr": "",
+            "en": "Have little object in my house, just those have really used.",
+            "tr": "",
+        },
+
+    },
+    {
+        "id": "ls2",
+        "image": [ph6, ph3],
+        "titre": {
+            "fr": "",
+            "en": " Travel ",
+            "tr": "",
+        },
+        "texte": {
+            "fr": "",
+            "en": "I love Travel. Travel allows me to try new things and meet new people, helping combat monotony.  Travel is good for my mental health.  Travel helps my brain function better and boosts creativity.",
+            "tr": "",
+        },
+    },
+    {
+        "id": "ls3",
+        "image": [ph12],
+        "titre": {
+            "fr": "",
+            "en": "Friends ",
+            "tr": "",
+        },
+        "texte": {
+            "fr": "",
+            "en": "I love speak, spend time with my friends.",
+            "tr": ""
+        }, 
+    },
+
+    {
+        "id": "ls4",
+        "image": ["https://images.pexels.com/photos/1914984/pexels-photo-1914984.jpeg?auto=compress&cs=tinysrgb&w=1200"],
+        "titre": {
+            "fr": "",
+            "en": "Family ",
+            "tr": "",
+        },
+        "texte": {
+            "fr": "",
+            "en": "Most importante thing in my life.",
+            "tr": ""
+        },
+        
+    },
+
+    {
+        "id": "ls5",
+        "image": ["https://images.pexels.com/photos/1479120/pexels-photo-1479120.jpeg?auto=compress&cs=tinysrgb&w=1200"],
+        "titre": {
+            "fr": "",
+            "en": "Read Book ",
+            "tr": "",
+        },
+        "texte": {
+            "fr": "",
+            "en": "10 benefits of reading: Mental Stimulation, Stress Reduction,  Knowledge, Vocabulary Expansion, Memory Improvement,  Stronger Analytical Thinking Skills, Improved Focus and Concentration, Better Writting Skills, Tranquillity,  Free Entertrainment "
+            ,"tr": ""
+        }, 
+    },
+
+    {
+        "id": "ls6",
+        "image": [ph13],
+        "titre": "Cats my love ",
+        "texte": {
+            "fr": "",
+            "en": "I love cats since I have a cat, it's just best thing in my life.", 
+            "tr": "",
+        },
+    },
+
+    {
+        "id": "ls7",
+        "image": [ph25, ph26, ph1],
+        "titre": {
+            "fr": "",
+            "en": "Inner World ",
+            "tr": "",
+        },
+        "texte": {
+            "fr": "",
+            "en": "He should retreat into his shell for a while and listen to himself thoroughly. He must withdraw into his inner world. Those who have no knowledge of their inner world, are only in love with matter and are lost in worldly pleasures."
+           , "tr": "",
+        },
+    },
+
+
+    {
+        "id": "ls8",
+        "image": [ph23, ph15],
+        "titre": {
+            "fr": "",
+            "en": "Study, Work, Be productive ",
+            "tr": "",
+        },
+        "texte": {
+            "fr": "",
+            "en": "If you choose a job you love, you will never have to work or be tired even for a moment in your life."
+        , "tr": ""
+        },
+    },
+
+
+    {
+        "id": "ls9",
+        "image": [ph22, ph19, ph20],
+        "titre": {
+            "fr": "",
+            "en": "Love",
+            "tr": "",
+        },
+        "texte": {
+            "fr": "",
+            "en": "There is something that I have kept silent in the blind wells of my heart, something that I have tried to keep away from myself, something that I cannot express, something that I ignore, something that I cover up... It is impossible to describe. But love was already madness. If you ask me to tell you, I cannot explain. I have an enthusiastic desire to seek, but I am also afraid of meeting. Maybe I'm not setting out on this path to find it. It seems like I have no other cure than searching... I'm not sure whether I'm going to look for her or trying to find myself. I can't be honest with myself. More precisely, I don't even know what's going on with me. Maybe Her, maybe myself, but I had to find that thing that was missing inside. “- Tell me more, I said.– Do you like it?– A lot. If I could, I would talk to you eight hundred and fifty-two thousand kilometers non-stop.– How do we get gasoline on all these roads?– We’ll pretend to go.” My Sweet Orange Tree Either all the poets loved you, or I find you in every verse. Erdem Bayezit",
+            "tr": "Bir şey var ki kalbimin kör kuyularında sessizliğe gömdüğüm, kendimden bile uzaklaştırmaya çalıştığım, dillendiremediğim, görmezden geldiğim, üstünü örttüğüm bir şey... Tarifi mümkün değil. Ama aşk da zaten bir delilikti işte. Anlat deseniz anlatamam. Coşkun bir aramak arzusu taşıyorum içimde, ama bir o kadar da korkuyorum kavuşmaktan. Belki de bulmak gayesiyle çıkmıyorum bu yola. Aramaktan başka ilacım yok sanki... Fesleğeni mi aramaya gidiyorum, kendimi mi bulmaya çalışıyorum emin değilim. Dürüst olamiyorum kendime karşı. Daha doğrusu ben bile bilmiyorum bende neler olup bittiğini. Belki Fesleğeni, belki de kendimi ama bulmalıydım içeride eksik olan o şeyi.",
+        },
+
+    },
+
+    {
+        "id": "ls10",
+        "image": [ph21, ph24],
+        "titre": {
+            "fr": "",
+            "en": "Activity to do"
+            ,
+            "tr": "",
+        },
+        "texte": {
+            "fr": "",
+            "en": "To Camp,",
+            "tr": "",
+        },
+
+    },
+]
+
+export default {
+    array_lifestyle,
+}
